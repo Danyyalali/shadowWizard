a5177a4bb657396d1155333168aabc80f0c5c4b5
Danyyalali <danyyal.ali@techverx.com>
2021-05-07 11:38:06 +0500

    files for the code to make nodule

---
+++ index.js
@@ -0,0 +1,21 @@
+function shadowWizard(options) {
+    let images = document.querySelectorAll('.shadowWizard');
+
+    if (options.shadow_type === 'hard')
+        options.shadow_type = '0px'
+    else
+        options.shadow_type = '15px'
+
+    images.forEach(Image => {
+        Image.style.boxShadow = '10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)';
+
+        if (options.padding) {
+            Image.style.padding = '1em';
+        }
+    })
+
+}
+
+
+
+module.exports.shadowWizard = shadowWizard;
